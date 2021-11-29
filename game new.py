a3a08354561632db1077a20d429a049343de9ad5
CowardlyCat2 <86519321+CowardlyCat2@users.noreply.github.com>
2021-11-29 17:21:12 +0000

    1.1.3

--- game new.py
+++ game new.py
@@ -1,4 +1,4 @@
-import random
+mport random
 #This gets the questions for the true or false quiz
 def get_tof_questions():
     statements = []
@@ -25,6 +25,21 @@ def get_questions():
     statements.append(["As of 2021, Clement Attlee remains the longest-serving leader of which British political party?","LABOUR"])
     statements.append(["Footballer Kenny Dalglish won 102 caps for which country?","SCOTLAND"])
     statements.append(["The English word 'aviation', meaning the flying or operation of aircraft, origins in which European language?","FRENCH"])
+    return statements
+
+#this gets the questions for the history quiz
+def get_history_questions():
+    statements = []
+    statements.append(["Who was Henry VIII's last wife?","CATHERINE PARR"])
+    statements.append(["Although never taking her seat, who was the first woman to be elected to the houses of parliament?","CONSTANCE MARKIEVICZ"])
+    statements.append(["Which English king died in 1066, leaving no heir to the throne?","EDWARD THE CONFESSOR"])
+    statements.append(["In which European country was there a civil war between 1946 and 1949?","GREECE"])
+    statements.append(["Which St Albans pub claims to be the oldest English pub?","YE OLDE FIGHTING"])
+    statements.append(["Who discovered penicillin?","ALEXANDER FLEMING"])
+    statements.append(["What year did the Titanic sink?","1912"])
+    statements.append(["Which Hertfordshire town had the first ever roundabout in the UK?","LETCHWORTH"])
+    statements.append(["Which new British military force was established in 1918?","RAF"])
+    statements.append(["Which Hertfordshire town was the first ever New Town after WWII?","STEVENAGE"])
     return statements
 
 #This is where the true of false quiz is played
@@ -56,6 +71,21 @@ def play_multiple():
             print("Incorrect, the correct answer was ",s[1])
     print("Your final score is",str(score)+"/5")
 
+#this is where the history quiz is played
+def play_history_quiz():
+    questions = get_history_questions()
+    random.shuffle(questions)
+    score = 0
+    for s in questions[:5]:
+        guess = input(s[0]+" ")
+        if guess.upper() == s[1]:
+            print("Correct")
+            score = score + 1
+        else:
+            print("Incorrect, the correct answer was ",s[1])
+    print("Your final score is",str(score)+"/5")
+    
+
 #this is where the easy maths game is played
 def play_easy_maths():
     print("\nPlease answer in whole numbers")
@@ -86,7 +116,7 @@ def play_easy_maths():
             else:
                 print("That was incorrect, the correct answer was",num1-num2)
         if opration == 4:
-            answer = int(input("The question is: "+str(num1)+" ÷ "+str(num2)+" "))
+            answer = int(input("The question is: "+str(num1)+" รท "+str(num2)+" "))
             if round(answer) == round(num1/num2):
                 print("That was correct")
                 score = score + 1
@@ -124,7 +154,7 @@ def play_hard_maths():
             else:
                 print("That was incorrect, the correct answer was",num1-num2)
         if opration == 4:
-            answer = int(input("The question is: "+str(num1)+" ÷ "+str(num2)+" "))
+            answer = int(input("The question is: "+str(num1)+" รท "+str(num2)+" "))
             if round(answer) == round(num1/num2):
                 print("That was correct")
                 score = score + 1
@@ -151,7 +181,7 @@ def calculator():
             elif choice == '3':
                 print(num1, "x", num2, "=",(num1*num2))
             elif choice == '4':
-                print(num1, "÷", num2, "=",(num1/num2))
+                print(num1, "รท", num2, "=",(num1/num2))
         else:
             print("invalid input")
         redo = input("Another calculation Y/n: ")
@@ -166,17 +196,14 @@ def main_menu():
     print("+--------------------------+")
     print("| Please select an option: |")
     print("|                          |")
-    print("| 1. True or false quiz    |")
-    print("| 2. Genral knowledge quiz |")
-    print("| 3. Maths menu            |")
+    print("| 1. Quiz Menu             |")
+    print("| 2. Maths menu            |")
     print("| 0. Quit                  |")
     print("+--------------------------+")
     choice = input("What do you want to do? ")
     if choice == "1":
-        play_tof_quiz()
+        menu = 3
     elif choice == "2":
-        play_multiple()
-    elif choice == "3":
         menu = 2
     elif choice == "0":
         print("Thanks for playing")
@@ -207,6 +234,30 @@ def maths_menu():
         menu = 1
     return menu
 
+#this is where the quizes menue is
+def quizes_menu():
+    menu = 3
+    print("+--------------------------+")
+    print("| Welcome to the quiz  menu|")
+    print("+--------------------------+")
+    print("| Please select an option: |")
+    print("|                          |")
+    print("| 1. True or false quiz    |")
+    print("| 2. Genral knowledge quiz |")
+    print("| 3. History quiz          |")
+    print("| 0. Back                  |")
+    print("+--------------------------+")
+    choice = input("What do you want to do? ")
+    if choice == "1":
+        play_tof_quiz()
+    elif choice == "2":
+        play_multiple()
+    elif choice == "3":
+        play_history_quiz()
+    elif choice == "0":
+        menu = 1
+    return menu
+
 #this is where the code is run to run the whole thing
 replay = 0
 menu = 1
@@ -217,6 +268,9 @@ while replay == 0:
     elif menu == 2:
         print("\n")
         menu = maths_menu()
+    elif menu == 3:
+        print("\n")
+        menu = quizes_menu()
     elif menu == 0:
         replay = 1
     else:
